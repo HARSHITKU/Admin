aaf1ecd01c81cd71370611f7aab91682f1ed0830
kishorerocks <babukishore@gmail.com>
2021-10-12 16:18:44 +0530

    completed video setup

--- src/app/components/features/admin/application-settings/video/new-video/new-video.component.ts
+++ src/app/components/features/admin/application-settings/video/new-video/new-video.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Video } from 'src/app/models/video';
+import { VideoService } from '../video.service';
 
 @Component({
   selector: 'app-new-video',
@@ -7,9 +12,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewVideoComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+  title: string = '';
+  buttonText: string = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<NewVideoComponent>,
+    private _snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private service: VideoService
+  ) {
+    this.form = this.fb.group({
+      videoURL: ['', Validators.required],
+    });
+    if (this.data.hasOwnProperty('_id')) {
+      this.title = 'Update Existing Video';
+      this.buttonText = 'Update Video';
+      this.setFormValue(this.data);
+    } else {  
+      this.title = 'Add New Video';
+      this.buttonText = 'Add Video';
+    }
+   }
+
+   ngOnInit(): void {
+  }
+
+  setFormValue(detailToUpdate: any) {
+    this.form.get('videoURL')?.setValue(detailToUpdate.videoURL);
+  }
+
+  closeDialog(message: string) {
+    this.dialogRef.close(message);
+  }
+
+  addUpdateDetails(data: Video) {
+    let newData = this.generatePayload(data);
+    if (this.title === 'Update Existing Video') {
+      this.service
+        .updateVideo(newData, this.data._id)
+        .subscribe((response) => {
+          if (response) {
+            this.openSnackBar('Video Updated Successfully');
+            this.closeDialog(response);
+          }
+        });
+    } else {
+      this.service.addVideo(newData).subscribe((response) => {
+        if (response) {
+          this.openSnackBar('Video Added Successfully');
+          this.closeDialog(response);
+        }
+      });
+    }
+  }
+
+  generatePayload(newDetail: any) {
+    let payload = {
+      videoURL: newDetail.videoURL,
+    };
+    return payload;
+  }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message, '', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: 3000
+    });
   }
 
 }
+
--- src/app/components/features/admin/application-settings/video/video.service.ts
+++ src/app/components/features/admin/application-settings/video/video.service.ts
@@ -1,9 +1,75 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CommonService } from 'src/app/services/common.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideoService {
 
-  constructor() { }
+  baseURL: string = '';
+
+  constructor(private http:HttpClient, private commonService: CommonService) {
+    this.baseURL = this.commonService.base_URL;
+   }
+
+   getAllVideo(): Observable<any> {
+     let token = localStorage.getItem('token');
+
+     const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      }),
+    };
+    const apiUrl = this.baseURL + 'admin/videos';
+    return this.http.get<any>(apiUrl, httpOptions);
+   }
+   getVideoById(userId: any): Observable<any> {
+    let token = localStorage.getItem('token');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      }),
+    };
+    const apiUrl = this.baseURL + 'admin/videos/' + userId;
+    return this.http.get<any>(apiUrl, httpOptions);
+  }
+  addVideo(videoData: any): Observable<any> {
+    let token = localStorage.getItem('token');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      }),
+    };
+    const apiUrl = this.baseURL + 'admin/videos';
+    return this.http.post(apiUrl, videoData, httpOptions);
+  }
+  deleteAbout(userId: string): Observable<any> {
+    let token = localStorage.getItem('token');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      }),
+    };
+    const apiUrl = this.baseURL + 'admin/videos/' + userId;
+    return this.http.delete(apiUrl, httpOptions);
+  }
+
+  updateVideo(aboutData: any, userId: string): Observable<any> {
+    let token = localStorage.getItem('token');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      }),
+    };
+    const apiUrl = this.baseURL + 'admin/videos/' + userId;
+    return this.http.patch(apiUrl, aboutData, httpOptions);
+  }
+ 
 }
