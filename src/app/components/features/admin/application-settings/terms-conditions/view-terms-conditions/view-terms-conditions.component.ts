45bacddffd7fed8d42fd65ea9c34ec213df4ed4f
kishorerocks <babukishore@gmail.com>
2021-10-21 08:15:09 +0530

    view changes

--- src/app/components/features/admin/application-settings/terms-conditions/view-terms-conditions/view-terms-conditions.component.ts
+++ src/app/components/features/admin/application-settings/terms-conditions/view-terms-conditions/view-terms-conditions.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-view-terms-conditions',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-terms-conditions.component.scss']
 })
 export class ViewTermsConditionsComponent implements OnInit {
+  title: string = "View";
+  form!: FormGroup;
+  buttonText: string = '';
+  userDetails: any;
+  isUpdate: boolean = false;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,public dialogRef: MatDialogRef<ViewTermsConditionsComponent> ,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    ){
+      this.form = this.fb.group({
+         
+        isDefault: [false, Validators.required]
+      });
+   }
+   ngOnInit(): void {
+    this.userDetails = this.data;
+  }
+  closeDialog(message: string) {
+    this.dialogRef.close(message);
+  }
+  
+  setFormValue(detailToUpdate: any) {
+     this.form.get('isDefault')?.setValue(detailToUpdate.isDefault);
   }
 
 }
